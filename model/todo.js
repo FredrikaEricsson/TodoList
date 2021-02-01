3c918a65954a473315f2b202cf3178d130b566cb
fredrikaericsson <fredrika@finnvid.se>
2021-02-01 15:33:42 +0100

    Created model for db

---
+++ model/todo.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 40 },
+  date: { type: Date, default: Date.now },
+});
+
+const Todo = mongoose.model("todo", todoSchema);
+module.exports = Todo;
