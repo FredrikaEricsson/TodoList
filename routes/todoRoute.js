15d9d735e393309fed57b41f5f472845074c7d5f
fredrikaericsson <fredrika@finnvid.se>
2021-02-03 16:02:36 +0100

    User can now add new todo that ends up in db. Also shows error page when input is empty

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const Todo = require("../model/todo");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log(req.query);
+  try {
+    const data = await Todo.find();
+    res.render("index.ejs", { data: data, error: "empty" });
+  } catch (err) {
+    res.render("error.ejs", { error: err });
+  }
+});
 
-  res.render("index.ejs");
+router.post("/", async (req, res) => {
+  console.log(req.body.name);
+  try {
+    await new Todo({
+      name: req.body.name,
+    }).save();
+    res.redirect("/");
+  } catch (err) {
+    res.render("error.ejs", { error: err });
+  }
 });
 
 module.exports = router;
